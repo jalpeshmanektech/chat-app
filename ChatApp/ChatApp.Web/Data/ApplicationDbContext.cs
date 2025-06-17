9a5c94994840f8ac3267e356bfab733ae3cb37fc
jalpeshmanektech <jalpeshnakummt@gmail.com>
2025-06-17 19:58:49 +0530

    Added demo project

---
+++ ChatApp/ChatApp.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatApp.Web.Data
+{
+     public class ApplicationDbContext : IdentityDbContext
+     {
+          public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+              : base(options)
+          {
+
+          }
+
+          protected override void OnModelCreating(ModelBuilder builder)
+          {
+               base.OnModelCreating(builder);
+
+               // Fix for MySQL: set max length for Identity columns
+               builder.Entity<IdentityRole>(entity => {
+                    entity.Property(m => m.Name).HasMaxLength(256);
+                    entity.Property(m => m.NormalizedName).HasMaxLength(256);
+               });
+               builder.Entity<IdentityUser>(entity => {
+                    entity.Property(m => m.UserName).HasMaxLength(256);
+                    entity.Property(m => m.NormalizedUserName).HasMaxLength(256);
+                    entity.Property(m => m.Email).HasMaxLength(256);
+                    entity.Property(m => m.NormalizedEmail).HasMaxLength(256);
+               });
+          }
+     }
+}
