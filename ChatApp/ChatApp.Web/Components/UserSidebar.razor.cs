295f8781daad89ae8ad032597ccedcef3b587d36
jalpeshmanektech <jalpeshnakummt@gmail.com>
2025-06-19 23:05:04 +0530

    changes media grid

--- ChatApp/ChatApp.Web/Components/UserSidebar.razor.cs
+++ ChatApp/ChatApp.Web/Components/UserSidebar.razor.cs
@@ -1,8 +1,14 @@
+using ChatApp.Web.ViewModels;
 using Microsoft.AspNetCore.Components;
 
 namespace ChatApp.Web.Components;
 
 public partial class UserSidebar : ComponentBase
 {
-     
+     [Parameter] public ChatUserVM? CurrentChatUserVM { get; set; }
+     [Parameter] public List<ChatMessage> ChatMessages { get; set; } = new();
+
+     protected List<ChatMessage> MediaItems => ChatMessages
+         .Where(m => !string.IsNullOrEmpty(m.ImageUrl) || !string.IsNullOrEmpty(m.FileUrl))
+         .ToList();
 } 
